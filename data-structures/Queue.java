1641f6b25e9ea3ff50332980861c7a58dc642d8d
Seunghwan_Lee <58278026+LeeSeunghwanSeungLee@users.noreply.github.com>
2022-05-06 22:30:34 +0900

    feat: Data-structures 추가
    Queue 기능 구현

---
+++ data-structures/Queue.java
@@ -0,0 +1,44 @@
+class Queue <T> {
+  private static MAX = 10000;
+  private int front;
+  private int rear;
+  T[] queue;
+  
+  public Queue() {
+    this.front = this.rear = 0;
+    queue = new int[MAX];
+  }
+  
+  public boolean isEmpty() {
+    return front == rear;
+  }
+  
+  public boolean isFull() {
+    return rear == (MAX - 1);
+  }
+  
+  public int size() {
+    return rear - front;
+  }
+  
+  public void push(T value) {
+    if (isFull())
+      return;
+    
+    queue[rear++] = value;
+  }
+  
+  public T pop() {
+    if (isEmpty())
+      return;
+    
+    return queue[front++];
+  }
+  
+  public T peek() {
+    if (isEmpty())
+      return null;
+    
+    return queue[front];
+  }
+}
